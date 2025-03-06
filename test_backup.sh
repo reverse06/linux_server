3893149e3955de88a9c340fef6a2e89e3335f3b0
Rain <palemoon06@protonmail.com>
2025-03-06 16:08:02 +0100

    Update test_backup.sh

--- test_backup.sh
+++ test_backup.sh
@@ -1,5 +1,7 @@
 #!/bin/bash
 
+echo "Début du test unitaire sur la backup..."
+
 #Définition des couleurs pour le compte-rendu
 GREEN="\033[32m"
 RED="\033[31m"
@@ -14,6 +16,7 @@ RESTORE_DIR="/tmp/www_restored"
 #Vérifier si le dossier source existe
 if [ ! -d "$SOURCE" ]; then
     echo -e "${RED}Le dossier source $SOURCE n'existe pas.${RESET}"
+    echo "Test unitaire sur la backup terminé pour cause d'échec."
     exit 1
 fi
 
@@ -24,6 +27,7 @@ cp -a "$SOURCE" "$BACKUP_DIR"
 #Vérification de la copie
 if [ ! -d "$BACKUP_DIR" ]; then
     echo -e "${RED}Échec de la copie temporaire.${RESET}"
+    echo "Test unitaire sur la backup terminé pour cause d'échec."
     exit 1
 fi
 
@@ -34,6 +38,7 @@ tar -czf "$BACKUP_FILE" -C "$BACKUP_DIR" .
 #Vérification de la sauvegarde
 if [ ! -f "$BACKUP_FILE" ]; then
     echo -e "${RED}Échec de la création de la sauvegarde.${RESET}"
+    echo "Test unitaire sur la backup terminé."
     exit 1
 fi
 
@@ -44,6 +49,7 @@ rm -rf "$BACKUP_DIR"
 #Vérification de la suppression de la copie temporaire
 if [ -d "$BACKUP_DIR" ]; then
     echo -e "${RED}Échec de la suppression de la copie temporaire.${RESET}"
+    echo "Test unitaire sur la backup terminé pour cause d'échec."
     exit 1
 fi
 
@@ -55,8 +61,11 @@ tar -xzf "$BACKUP_FILE" -C "$RESTORE_DIR"
 #Vérification de la restauration
 if [ ! "$(ls -A "$RESTORE_DIR")" ]; then
     echo -e "${RED}Échec de la restauration des fichiers.${RESET}"
+    echo "Test unitaire sur la backup terminé pour cause d'échec."
     exit 1
 fi
 
 #Compte-rendu
 echo -e "${GREEN}Si vous êtes arrivé jusqu'ici, c'est que le test a été passé avec succès!.${RESET}"
+
+echo "Test unitaire sur la backup terminé."
