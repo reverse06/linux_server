9480ea019491af43de78eeab9a057ae9a73d8aa0
Rain <palemoon06@protonmail.com>
2025-03-06 15:49:03 +0100

    Update master-config.sh

--- master-config.sh
+++ master-config.sh
@@ -39,7 +39,7 @@ sudo sed -i 's/^Port 22/Port 2025/' /etc/ssh/sshd_config
 
 sudo systemctl restart sshd
 
-echo "Service SSH configuré. Le port est 2025."
+echo "Service SSH configuré. Le port dédié est 2025."
 
 #------------------------------Fin du service SSH
 #------------------------------------------------
